910af28cae5447097fbc31304cc6019229f6a997
ChiefLopes <chieflopesekede@gmail.com>
2023-10-03 14:40:07 +0100

    added an onclick event to the Oauth button

--- src/Components/Sections/OAuth.jsx
+++ src/Components/Sections/OAuth.jsx
@@ -1,8 +1,13 @@
 import { FcGoogle } from "react-icons/fc";
 
 const OAuth = () => {
+    const onGoogleClick = (e) => {
+        e.preventDefault();
+        
+        console.log("Ogadimma");
+    }
   return (
-    <button className="flex items-center justify-center w-full uppercase  px-7 py-2 text-sm font-medium rounded border border-gray-300 rounded-md bg-red-600 hover:bg-red-700 active:bg-red-900 text-white shadow-md hover:shadow-lg active:shadow-lg transition ease-in-out duration-300">
+    <button type="" onClick={onGoogleClick} className="flex items-center justify-center w-full uppercase  px-7 py-2 text-sm font-medium rounded border border-gray-300 rounded-md bg-red-600 hover:bg-red-700 active:bg-red-900 text-white shadow-md hover:shadow-lg active:shadow-lg transition ease-in-out duration-300">
       <FcGoogle className="text-2xl mr-2 bg-white rounded-full" /> Continue with
       Google
     </button>
