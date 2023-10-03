aad91c5ab882ca6e2cc5e3319bf4de18709b6e1f
ChiefLopes <chieflopesekede@gmail.com>
2023-10-03 14:22:39 +0100

    added an error toast message

--- src/Components/Pages/SignUp.jsx
+++ src/Components/Pages/SignUp.jsx
@@ -51,7 +51,8 @@ const SignUp = () => {
       delete formDataCopy.password;
       formDataCopy.timestamp = serverTimestamp();
 
-      await setDoc(doc(db, "users", user.uid), formDataCopy);
+        await setDoc(doc(db, "users", user.uid), formDataCopy);
+        // toast.success("Sign up successful")
       navigate("/");
     } catch (error) {
      toast.error("Something went wrong!")
