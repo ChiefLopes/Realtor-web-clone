da986e25236dd9f518eb8de1f19919ac1ff893bf
ChiefLopes <chieflopesekede@gmail.com>
2023-10-13 10:32:41 +0100

    worked on the OAuth component trying to make the google authentification wirk

--- src/Components/Sections/OAuth.jsx
+++ src/Components/Sections/OAuth.jsx
@@ -35,9 +35,9 @@ const OAuth = () => {
 
   return (
     <button
-      type="button" 
-          onClick={onGoogleClick}
-      className="flex items-center justify-center w-full uppercase px-7 py-2 text-sm font-medium rounded border border-gray-300 rounded-md bg-red-600 hover:bg-red-700 active:bg-red-900 text-white shadow-md hover:shadow-lg active:shadow-lg transition ease-in-out duration-300 ptr-none">
+      type="button"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full uppercase px-7 py-2 text-sm font-medium rounded border border-gray-300 rounded-md bg-red-600 hover:bg-red-700 active:bg-red-900 text-white shadow-md hover:shadow-lg active:shadow-lg transition ease-in-out duration-300">
       <FcGoogle className="text-2xl mr-2 bg-white rounded-full" /> Continue with
       Google
     </button>
