b392890db31d19f14f536e937293f7aa5904fbe2
ChiefLopes <chieflopesekede@gmail.com>
2023-10-15 14:11:51 +0100

    finished wiith the UI section of the profile page, added a private route

--- src/Components/Pages/Profile.jsx
+++ src/Components/Pages/Profile.jsx
@@ -1,9 +1,11 @@
+import { getAuth } from "firebase/auth";
 import { useState } from "react";
 
 const Profile = () => {
+    const auth = getAuth();
   const [formData, setFormData] = useState({
-    name: "Louis Peace",
-    email: "chieflopesekede@gmail.com",
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
   });
 
   const { name, email } = formData;
@@ -19,7 +21,7 @@ const Profile = () => {
               id="name"
               value={name}
               disabled
-              className="w-full mb-6 px-4 py-2 text-xl text-zinc-700 bg-white border-2 border-grey-300 rounded transition ease-in-out"
+              className="w-full mb-6 px-4 py-2 text-xl  text-zinc-700 bg-white border-2 border-grey-300 rounded transition ease-in-out"
             />
 
             {/* email input */}
@@ -29,12 +31,19 @@ const Profile = () => {
               value={email}
               disabled
               className="w-full mb-6 px-4 py-2 text-xl text-zinc-700 bg-white border-2 border-grey-300 rounded transition ease-in-out"
-                      />
-                      
-                      <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
-                          <p className="flex items-center">Do you want to change your name? <span className="text-red-500 ml-2  hover:text-red-700 cursor-pointer transition ease-in-out duration-600">Edit</span></p>
-                          <p className="text-blue-600 hover:text-blue-800 cursor-pointer transition ease-in-out duration-600">sign out</p>
-                      </div>
+            />
+
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p className="flex items-center">
+                Do you want to change your name?
+                <span className="text-red-500 ml-2  hover:text-red-700 cursor-pointer transition ease-in-out duration-600">
+                  Edit
+                </span>
+              </p>
+              <p className="text-blue-600 hover:text-blue-800 cursor-pointer transition ease-in-out duration-600">
+                sign out
+              </p>
+            </div>
           </form>
         </div>
       </section>
