59777054074048053f1ba4df00535b74135b6032
ChiefLopes <chieflopesekede@gmail.com>
2023-11-07 19:51:12 +0100

    implemented an onchange event which allows the state of the input to change so that it can be edited

--- src/Components/Pages/Profile.jsx
+++ src/Components/Pages/Profile.jsx
@@ -3,21 +3,27 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 
 const Profile = () => {
-    const auth = getAuth();
-    const navigate = useNavigate();
+  const auth = getAuth();
+  const navigate = useNavigate();
     const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
-    email: auth.currentUser.email
+    email: auth.currentUser.email,
   });
 
   const { name, email } = formData;
-   
-    function onLogOut (){
-        auth.signOut();
-        navigate("/")
+
+  function onLogOut() {
+    auth.signOut();
+    navigate("/");
+  }
+    function onChange(e) {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]:e.target.value,
+        }))
     }
-    
+
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -30,6 +36,7 @@ const Profile = () => {
               id="name"
               value={name}
               disabled={!changeDetail}
+              onChange={onChange}
               className="w-full mb-6 px-4 py-2 text-xl  text-zinc-700 bg-white border-2 border-grey-300 rounded transition ease-in-out"
             />
 
@@ -39,17 +46,22 @@ const Profile = () => {
               id="email"
               value={email}
               disabled={!changeDetail}
+              onChange={onChange}
               className="w-full mb-6 px-4 py-2 text-xl text-zinc-700 bg-white border-2 border-grey-300 rounded transition ease-in-out"
             />
 
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
               <p className="flex items-center">
                 Do you want to change your name?
-                <span onClick={() => setChangeDetail((prevState) => !prevState)} className="text-red-500 ml-2  hover:text-red-700 cursor-pointer transition ease-in-out duration-600">
-                 {changeDetail ? "Apply Change" : "Edit"}
+                <span
+                  onClick={() => setChangeDetail((prevState) => !prevState)}
+                  className="text-red-500 ml-2  hover:text-red-700 cursor-pointer transition ease-in-out duration-600">
+                  {changeDetail ? "Apply Change" : "Edit"}
                 </span>
               </p>
-              <p onClick={onLogOut} className="text-blue-600 hover:text-blue-800 cursor-pointer transition ease-in-out duration-600">
+              <p
+                onClick={onLogOut}
+                className="text-blue-600 hover:text-blue-800 cursor-pointer transition ease-in-out duration-600">
                 sign out
               </p>
             </div>
