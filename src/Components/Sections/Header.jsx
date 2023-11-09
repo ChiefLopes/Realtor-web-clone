1513cbfa2f788c9e6a50a8488bad2a48fc5cb140
ChiefLopes <chieflopesekede@gmail.com>
2023-11-09 05:46:53 +0100

    modified the header section for the sign in, downlaoded and added the spinner svg for loading of the site

--- src/Components/Sections/Header.jsx
+++ src/Components/Sections/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   const location = useLocation();
     const navigate = useNavigate();
     const auth = getAuth()
-    useEffect(() => {
+   useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
             setPageState("Profile")
---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,9 @@
+import Spinner from "../assets/svg/Spinner.svg"
+const Spinner = () => {
+    
+  return (
+    <div>Spinner</div>
+  )
+}
+
+export default Spinner
