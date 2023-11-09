ad7689d52b824bc00b6c23783ae2423164ed48ec
ChiefLopes <chieflopesekede@gmail.com>
2023-11-09 13:34:33 +0100

    On the button, i added a type, id and value so that i can use to style both buttons dynamiclly

--- src/Components/Pages/CreateListing.jsx
+++ src/Components/Pages/CreateListing.jsx
@@ -1,9 +1,22 @@
 
 
-const  CreateListing = () => {
+const CreateListing = () => {
+    function onChange(e) {
+         
+    }
+    
   return (
-    <div>CreateListing</div>
-  )
+    <main className="max-w-md mx-auto px-2">
+          <h1 className="text-3xl font-bold text-center mt-4 ">Create a Listing</h1>
+          <form>
+              <p className="text-lg font-semibold mt-6">Sell / Rent</p>
+          </form>
+          <div>
+              <button type="button" id="type" value="sale" onClick={onchange} className={``}>Sell</button>
+              <button type="button" id="type" value="rent" onClick={onchange}>Rent</button>
+          </div>
+    </main>
+  );
 }
 
 export default CreateListing
