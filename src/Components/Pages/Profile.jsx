08e813bd8e652ce9ca6632173f3325b024f12b45
ChiefLopes <chieflopesekede@gmail.com>
2023-11-09 12:37:28 +0100

    at line 99, after adding stylings to the button, i added some edits to the Home image at line 100, insinde the button. used rounded-full, and also p-1 to add rounded backgroud to the Home icon and also add padding between the icon and the background

--- src/Components/Pages/Profile.jsx
+++ src/Components/Pages/Profile.jsx
@@ -96,9 +96,9 @@ const Profile = () => {
             </div>
           </form>
 
-          <button type="submit" className="w-full bg-blue-600 rounded text-white uppercase px-7 py-2 text-sm font-medium">
-            <Link to="/create-listing" >
-              <FcHome /> Sell Or Rent Your Home
+          <button type="submit" className="w-full bg-blue-600 rounded text-white uppercase px-7 py-2 text-sm font-medium shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 ">
+            <Link to="/create-listing"className="flex justify-center items-center" >
+              <FcHome className="mr-2 text-3xl bg-red-300 rounded-full p-1"/> Sell Or Rent Your Home
             </Link>
           </button>
         </div>
