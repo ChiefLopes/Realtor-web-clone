b5b8569d74a18b21977103299b59a9faaacc4ed6
ChiefLopes <chieflopesekede@gmail.com>
2023-11-10 03:23:31 +0100

    mind you, the maxLength and minLength properties are html form properties which would throw an error if the inputed value is more than 30 characters or less than 10. I as well added 'required' to the input so that the form won't submit if the input has no value

--- src/Components/Pages/CreateListing.jsx
+++ src/Components/Pages/CreateListing.jsx
@@ -49,10 +49,11 @@ const CreateListing = () => {
           id="name"
           value={name}
           onChange={onChange}
-          placeholder="Full Name"
+          placeholder="Property Name"
           maxLength="32"
           minLength="10"
-          className="w-full mb-6 px-2 py-2 mt-2 text-md  text-zinc-700 bg-white border-2 border-blue-300 rounded transition ease-in-out outline-none "
+          required
+          className="w-full mb-6 px-4 py-2 mt-2 text-md  text-zinc-700 bg-white border-2 border-blue-300 rounded transition ease-in-out outline-none "
         />
       </form>
     </main>
