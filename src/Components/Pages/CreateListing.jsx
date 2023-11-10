d64d32722f70cbd0040ed49c3918e456eba5bf17
ChiefLopes <chieflopesekede@gmail.com>
2023-11-10 04:01:48 +0100

    added two number inputs for bed and bathrooms. set their initial state valyes to be one apiece. then the minimum number to be inputted is 1 while maximum is 50

--- src/Components/Pages/CreateListing.jsx
+++ src/Components/Pages/CreateListing.jsx
@@ -4,9 +4,11 @@ const CreateListing = () => {
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
+    bedrooms: 1,
+    bathrooms: 1,
   });
 
-  const { type, name } = formData;
+  const { type, name, bedrooms, bathrooms } = formData;
 
   function onChange(e) {}
 
@@ -55,6 +57,29 @@ const CreateListing = () => {
           required
           className="w-full mb-6 px-4 py-2 mt-2 text-md  text-grey-700 bg-white border-2 border-grey-300 rounded transition ease-in-out outline-none focus:text-grey-700 focus:bg-white focus:border-slate-600 mb-6"
         />
+
+        <div className="flex space-x-7">
+          <div>
+            <p className="text-lg font-semibold">Bedrooms</p>
+            <input
+              type="number"
+              id="bedrooms"
+              value={bedrooms}
+                          className="w-20 h-8 outline-none text-center"
+                          min="1" max="20"              onChange={onChange}
+            />
+          </div>
+          <div>
+            <p className="text-lg font-semibold">Bathrooms</p>
+            <input
+              type="number"
+              id="bathrooms"
+              value={bathrooms}
+              className="w-20 h-8 outline-none text-center"
+              min="1" max="20"              onChange={onChange}
+            />
+          </div>
+        </div>
       </form>
     </main>
   );
