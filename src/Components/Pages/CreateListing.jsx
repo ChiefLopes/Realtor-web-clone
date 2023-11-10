bdf4f63c889a965f8e5f4c36acc058ee989aaa1b
ChiefLopes <chieflopesekede@gmail.com>
2023-11-10 03:07:33 +0100

    created an input for name and also added its value to be name

--- src/Components/Pages/CreateListing.jsx
+++ src/Components/Pages/CreateListing.jsx
@@ -1,30 +1,53 @@
 import { useState } from "react";
 
 const CreateListing = () => {
-    const [formData, setFormData] = useState({
-        type: "rent",
-    })
-    
-    const { type } = formData;
-    
-    function onChange(e) {
-         
-    }
-    
+  const [formData, setFormData] = useState({
+    type: "rent",
+    name: "",
+  });
+
+  const { type, name } = formData;
+
+  function onChange(e) {}
+
   return (
     <main className="max-w-md mx-auto px-2">
-          <h1 className="text-3xl font-bold text-center mt-4 ">Create a Listing</h1>
-          <form>
-              <p className="text-lg font-semibold mt-6">Sell / Rent</p>
-          </form>
-          <div className="flex py-4">
-              <button type="button" id="type" value="sale" onClick={onchange} className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg  active:shadow-lg transition duration-150 ease-in-out w-full ${type === "rent" ? "bg-white text-black" : "bg-slate-600 text-white"}`}>Sell</button>
-              
-              <button type="button" id="type" value="sale" onClick={onchange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg  active:shadow-lg transition duration-150 ease-in-out w-full ${type === "sale" ? "bg-white text-black" : "bg-slate-600 text-white" }`}>Rent</button>
-               
-          </div>
+      <h1 className="text-3xl font-bold text-center mt-4 ">Create a Listing</h1>
+      <form>
+        <p className="text-lg font-semibold mt-6">Sell / Rent</p>
+
+        <div className="flex py-4">
+          <button
+            type="button"
+            id="type"
+            value="sale"
+            onClick={onchange}
+            className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg  active:shadow-lg transition duration-150 ease-in-out w-full ${
+              type === "rent"
+                ? "bg-white text-black"
+                : "bg-slate-600 text-white"
+            }`}>
+            Sell
+          </button>
+
+          <button
+            type="button"
+            id="type"
+            value="sale"
+            onClick={onchange}
+            className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg  active:shadow-lg transition duration-150 ease-in-out w-full ${
+              type === "sale"
+                ? "bg-white text-black"
+                : "bg-slate-600 text-white"
+            }`}>
+            Rent
+          </button>
+        </div>
+        <p className="text-lg font-semibold">Name</p>
+        <input type="text" id="name" value={name} />
+      </form>
     </main>
   );
-}
+};
 
-export default CreateListing
+export default CreateListing;
